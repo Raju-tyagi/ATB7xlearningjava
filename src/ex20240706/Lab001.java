867d1385820050773975bdde6bc056cc57cb9b48
Raju-tyagi <goalachiver.raju@gmail.com>
2024-07-16 09:14:27 +0530

    all files

--- src/ex20240706/Lab001.java
+++ src/ex20240706/Lab001.java
@@ -1,2 +1,7 @@
-package ex20240706;public class Lab001 {
+package ex20240706;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
